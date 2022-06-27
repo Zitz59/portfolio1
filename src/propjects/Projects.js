ddef454d64f228e6e59e353e4d08fc0347c907e0
Zitz59 <dondongudon@gmail.com>
2022-06-27 16:29:10 +0300

    add  Projects component

--- src/propjects/Projects.js
+++ src/propjects/Projects.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import styles from './Projects.module.css'
+import stylesContainer from "../common/styles/Container.module.css"
+import {Project} from "./project/Project";
+
 const Projects = () => {
     return (
         <div className={styles.projectBlock}>
+            <div className={`${stylesContainer.container} ${styles.projectContainer}`}>
+                <h2 className={styles.projectsTitle}>Projects</h2>
+                <div className={styles.projects}>
+                    <Project projectTitle={"Social Network"}
+                             projectDescription={"Architecto cupiditate debitis dignissimos ea, exercitationem expedita iusto odio quaerat sequi voluptates? Exercitationem, possimus."}/>
+                    <Project projectTitle={"Todolist"}
+                             projectDescription={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid eligendi excepturi facere nostrum quam quisquam vel."}/>
+                </div>
+            </div>
 
         </div>
     );
