d6cbb5d59b562c4d2daa9c09f9e0043f766671ce
Zitz59 <dondongudon@gmail.com>
2022-06-27 16:29:46 +0300

    fix style names in component & module

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -7,7 +7,7 @@ export const Skills = () => {
     return (
         <div className={styles.skillsBlock}>
             <div className={`${stylesContainer.container} ${styles.skillsContainer}`}>
-                <h2 className={styles.title}>Skills</h2>
+                <h2 className={styles.skillsTitle}>Skills</h2>
                 <div className={styles.skills}>
                     <Skill title = {"JS"} description={ "Ad consectetur cumque cupiditate deleniti est et harum id magni placeat suscipit tempora, voluptatum."}/>
                     <Skill title = {"CSS"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dicta esse ipsa magni nostrum possimus voluptatum?"}/>
