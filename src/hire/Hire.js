ee051c4c945752de57808ac756556aa950e4cf6c
Zitz59 <dondongudon@gmail.com>
2022-06-27 16:48:10 +0300

    add Hire component

---
+++ src/hire/Hire.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from "./Hire.module.css"
+import stylesContainer from '../common/styles/Container.module.css'
+
+export const Hire = () => {
+    return (
+        <div className={styles.hireBlock}>
+            <div className={`${stylesContainer.container} ${styles.hireContainer}`}>
+                <h3 className={styles.hireTitle}>looking into remote work options or relocate</h3>
+                <button className={styles.hireButton}>Hire me</button>
+            </div>
+        </div>
+    );
+};
+
