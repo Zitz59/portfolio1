4156885ed28e7c62bef0a0cf790233a842b7cf76
Zitz59 <dondongudon@gmail.com>
2022-06-27 20:03:09 +0300

    connect new components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-
 import './App.css';
 import {Header} from "./header/Header";
 import {Main} from "./main/Main";
 import {Skills} from "./skills/Skills";
+import Projects from "./propjects/Projects";
+import {Hire} from "./hire/Hire";
+import {Contacts} from "./contacts/Contacts";
+import Footer from "./footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-        <Main/>
-        <Skills/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Main/>
+            <Skills/>
+            <Projects/>
+            <Hire/>
+            <Contacts/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
