c0e5fd6223781b9408823d78dd6e4b6f7ca5e6af
Zitz59 <dondongudon@gmail.com>
2022-06-27 20:01:47 +0300

    add Icon component

---
+++ src/icon/Icon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./Icon.module.css"
+
+
+export const Icon = () => {
+    return (
+        <div className={styles.iconContainer}>
+            <div className={styles.icon}></div>
+        </div>
+    );
+};
+
