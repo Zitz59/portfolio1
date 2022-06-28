5364352ead4a20c0080e911e195fc1797a907a46
Zitz59 <dondongudon@gmail.com>
2022-06-28 13:32:25 +0300

    refactor

--- src/icon/Icon.js
+++ src/icon/Icon.js
@@ -4,9 +4,7 @@ import styles from "./Icon.module.css"
 
 export const Icon = () => {
     return (
-        <div className={styles.iconContainer}>
             <div className={styles.icon}></div>
-        </div>
     );
 };
 
