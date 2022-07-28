834a873e52c64c6e4dd06b4ab06569485768d45a
Zitz59 <dondongudon@gmail.com>
2022-07-28 15:51:36 +0300

    feat:add universal title

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -2,12 +2,13 @@ import React from "react";
 import styles from "./Skills.module.css"
 import stylesContainer from "../common/styles/Container.module.css"
 import {Skill} from "./skill/Skill";
+import {Title} from "../common/components/title/Title";
 
 export const Skills = () => {
     return (
         <div className={styles.skillsBlock}>
             <div className={`${stylesContainer.container} ${styles.skillsContainer}`}>
-                <h2 className={styles.skillsTitle}>Skills</h2>
+                <Title text={'Skills'}/>
                 <div className={styles.skills}>
                     <Skill title = {"JS"} description={ "Ad consectetur cumque cupiditate deleniti est et harum id magni placeat suscipit tempora, voluptatum."}/>
                     <Skill title = {"CSS"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dicta esse ipsa magni nostrum possimus voluptatum?"}/>
