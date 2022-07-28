c7b992cbeb7c74fb8e5a489352a6eef2290b84c5
Zitz59 <dondongudon@gmail.com>
2022-07-28 15:49:06 +0300

    feat:add universal button refactor:delete unused styles

--- src/hire/Hire.js
+++ src/hire/Hire.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from "./Hire.module.css"
 import stylesContainer from '../common/styles/Container.module.css'
+import {Button} from "../common/components/button/Button";
 
 export const Hire = () => {
     return (
         <div className={styles.hireBlock}>
             <div className={`${stylesContainer.container} ${styles.hireContainer}`}>
                 <h3 className={styles.hireTitle}>Looking into remote work options or relocate</h3>
-                <button className={styles.hireButton}>Hire me</button>
+                <Button buttonTitle={'Hire me'}/>
             </div>
         </div>
     );
