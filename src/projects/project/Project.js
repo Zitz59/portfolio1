a40b9707db71bc6cd359857d8c3f088a4eb19e4b
Zitz59 <dondongudon@gmail.com>
2022-07-28 15:50:05 +0300

    feat:change path to file

---
+++ src/projects/project/Project.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from "./Project.module.css"
+import {Button} from "../../common/components/button/Button";
+
+export const Project = (props) => {
+    return (
+        <div className={styles.project}>
+            <div style={props.style} className={styles.projectPic}>
+                <Button buttonTitle={'Watch'}/>
+            </div>
+            <div className={styles.projectContent}><h4 className={styles.projectTitle}>{props.projectTitle}</h4>
+                <span className={styles.projectDesc}>{props.projectDescription}</span>
+            </div>
+        </div>
+    );
+};
+
