1fa3905ad96c63ed485534cec436cc37ce8853b4
Zitz59 <dondongudon@gmail.com>
2022-07-28 15:54:13 +0300

    feat: change path

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import {Header} from "./header/Header";
 import {Main} from "./main/Main";
 import {Skills} from "./skills/Skills";
-import Projects from "./propjects/Projects";
+import Projects from "./projects/Projects";
 import {Hire} from "./hire/Hire";
 import {Contacts} from "./contacts/Contacts";
 import Footer from "./footer/Footer";
