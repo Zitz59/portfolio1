127de43ced61d594fda9ead67495158eb32d3da8
Zitz59 <dondongudon@gmail.com>
2022-08-07 18:52:33 +0300

    feat: add icons

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import styles from "./Footer.module.css"
+import styles from "./Footer.module.scss"
 import {Icon} from "../icon/Icon";
+import instIc from "../assets/icons/instagram-social-network-logo-of-photo-camera-svgrepo-com.svg";
+import githubIc from "../assets/icons/github-svgrepo-com.svg";
+import fbIc from "../assets/icons/facebook-svgrepo-com.svg";
+import lnIc from "../assets/icons/linkedin-logo-svgrepo-com.svg";
 
 
 const Footer = () => {
+    const instIcon = {
+        backgroundImage:`url(${instIc})`
+    }
+    const gitIcon = {
+        backgroundImage:`url(${githubIc})`
+    }
+    const fbIcon = {
+        backgroundImage:`url(${fbIc})`
+    }
+    const lnIcon = {
+        backgroundImage:`url(${lnIc})`
+    }
+
     return (
         <div className={styles.footerBlock}>
             <div className={styles.footerContainer}>
                 <h4 className={styles.footerTitle}>Alexander Zaytsev</h4>
                 <div className={styles.iconsBlock}>
-                    <Icon/>
-                    <Icon/>
-                    <Icon/>
-                    <Icon/>
+                    <Icon style={instIcon}/>
+                    <Icon style={gitIcon}/>
+                    <Icon style={fbIcon}/>
+                    <Icon style={lnIcon}/>
                 </div>
                 <h5> &copy; 2022 All rights reserved</h5>
             </div>
