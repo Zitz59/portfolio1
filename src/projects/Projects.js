f6968d81e79c75248d9dad6d2a558177e293855b
Zitz59 <dondongudon@gmail.com>
2022-10-07 21:34:54 +0300

    refactor: change imports add props

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,32 +1,40 @@
 import React from 'react';
-import styles from './Projects.module.css'
+import styles from './Projects.module.scss'
 import stylesContainer from '../common/styles/Container.module.css'
 import {Project} from './project/Project';
 import {Title} from '../common/components/title/Title';
 import snetImg from '../assets/images/socialNtwrk.png'
 import countImg from '../assets/images/counter.png'
 
-const Projects = (props) => {
-    const socialNet = {
-        backgroundImage:`url(${snetImg})`
+const Projects = () => {
+    const socialNetwork = {
+        backgroundImage: `url(${snetImg})`
     };
 
     const counter = {
-        backgroundImage:`url(${countImg})`
+        backgroundImage: `url(${countImg})`
     };
 
     return (
         <div className={styles.projectBlock}>
-            <div className={`${stylesContainer.container} ${styles.projectContainer}`}>
-                <Title text={'Projects'}/>
-                <div className={styles.projects}>
-                    <Project style = {socialNet} projectTitle={"Social Network"}
-                             projectDescription={"Architecto cupiditate debitis dignissimos ea."}/>
-                    <Project style={counter} projectTitle={"Counter"}
-                             projectDescription={"Lorem ipsum dolor sit amet."}/>
+            <div className={styles.row}>
+                <div className={styles.sideColumn}>
+                    <h2>
+                        <span className={styles.baffleText}>
+                            <span className={styles.baffleSpan}>Projects</span>
+                        </span>
+                    </h2>
+                </div>
+                <div className={`${stylesContainer.container} ${styles.projectContainer}`}>
+                    <Title text={'Projects'}/>
+                    <div className={styles.projects}>
+                        <Project style={socialNetwork} projectTitle={"Social Network"}
+                                 projectDescription={"Architecto cupiditate debitis dignissimos ea."}/>
+                        <Project style={counter} projectTitle={"Counter"}
+                                 projectDescription={"Lorem ipsum dolor sit amet."}/>
+                    </div>
                 </div>
             </div>
-
         </div>
     );
 };
