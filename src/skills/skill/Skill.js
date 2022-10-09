8bfa418c5305712b371de1aac409639be8163487
Zitz59 <dondongudon@gmail.com>
2022-10-09 20:03:35 +0300

    refactor: change imports add props

--- src/skills/skill/Skill.js
+++ src/skills/skill/Skill.js
@@ -1,13 +1,12 @@
 import React from "react";
-import styles from "./Skill.module.css"
+import styles from "./Skill.module.scss"
 
 export const Skill = (props) => {
     return (
-
         <div className={styles.skill}>
-            <div className={styles.icon}></div>
+            <div style = {props.style} className={styles.icon}></div>
             <h3>{props.title}</h3>
-            <span className={styles.description}> {props.description}</span>
+            <span className={styles.description}>{props.description}</span>
         </div>
     )
 
