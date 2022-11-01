7be03dc317ee97ac24f0679045fe2082874311b8
Zitz59 <dondongudon@gmail.com>
2022-11-02 01:14:32 +0300

    refactor: add classNamefor button block

--- src/common/components/button/Button1.js
+++ src/common/components/button/Button1.js
@@ -2,7 +2,7 @@ import styles from './Button1.module.scss'
 
 export const Button1 = (props) => {
     return (
-        <div>
+        <div className={styles.buttonBlock}>
             <button className={styles.button1} onClick={()=>{}}>{props.buttonTitle}</button>
         </div>
     )
