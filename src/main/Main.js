f2581d98ae98750561a244a5e367b9f9e98436d7
Zitz59 <dondongudon@gmail.com>
2025-01-11 11:04:08 +0100

    add profile photo

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,16 +1,23 @@
 import React from "react";
 import styles from "./Main.module.scss"
+import profileImg from '../assets/images/profilePhoto.jpg'
 
 export const Main = () => {
+
+    const profilePhoto = {
+        backgroundImage: `url(${profileImg})`,
+    }
+
     return (
         <div className={styles.mainBlock}>
             <div className={styles.container}>
                 <div className={styles.mainDesc}>
                     <span>Hello</span>
-                    <h1>I'm Alexander Zaytsev</h1>
+                    <h1>I'm Alexander Zaitsev</h1>
                     <p>A Frontend Developer</p>
                 </div>
-                <div className={styles.mainPhoto}></div>
+                <div style={profilePhoto} className={styles.mainPhoto}>
+                </div>
             </div>
         </div>
     )
