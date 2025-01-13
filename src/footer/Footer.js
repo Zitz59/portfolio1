d2b827d16d9ed14ad52ecb11f8f535890c30b34f
Zitz59 <dondongudon@gmail.com>
2025-01-13 22:42:42 +0100

    change text errors

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -20,17 +20,20 @@ const Footer = () => {
         backgroundImage: `url(${lnIc})`
     }
 
+    //TODO: 1 --- добавить анимацию при наведении мышки на логотипы
+    //TODO: 2 --- добавить рабочие ссылки при клике на которые будет перекидывать на linkedin и github
+
     return (
         <div className={styles.footerBlock}>
             <div className={styles.footerContainer}>
-                <h4 className={styles.footerTitle}>Alexander Zaytsev</h4>
+                <h4 className={styles.footerTitle}>Alexandr Zaitsev</h4>
                 <div className={styles.iconsBlock}>
                     <Icon style={instIcon}/>
                     <Icon style={gitIcon}/>
                     <Icon style={fbIcon}/>
                     <Icon style={lnIcon}/>
                 </div>
-                <h5> &copy; 2022 All rights reserved</h5>
+                <h5> &copy; 2025 All rights reserved</h5>
             </div>
         </div>
     );
