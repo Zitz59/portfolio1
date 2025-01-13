49ef8b4a914034b7ea2737e1b2ee3d92e9ec0692
Zitz59 <dondongudon@gmail.com>
2025-01-13 22:43:54 +0100

    add navigation links

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './Nav.module.css'
 
+//TODO 2---добавить свертывание меню в "гармошку" при уменьшении страницы в ширину (scss)
+
 export const Nav = () => {
     return (
         <div className={styles.nav}>
-            <a className={styles.navLink} href="">Home</a>
-            <a className={styles.navLink} href="">Skills</a>
-            <a className={styles.navLink} href="">Projects</a>
-            <a className={styles.navLink} href="">Contacts</a>
+            <a className={styles.navLink} href="#home">Home</a>
+            <a className={styles.navLink} href="#skills">Skills</a>
+            <a className={styles.navLink} href="#projects">Projects</a>
+            <a className={styles.navLink} href="#contacts">Contacts</a>
         </div>
     )
 }
